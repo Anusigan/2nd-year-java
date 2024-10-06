f987192e18484300dce25c35aad2715b13e372e9
Anusigan Sivananthan <148673260+Anusigan@users.noreply.github.com>
2024-10-06 22:06:39 +0530

    excercises

---
+++ src/q1.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+public class q1 {
+public static void main (String [] args){
+    Scanner sc=new Scanner (System.in);
+    int [] marks=new int[6];
+    for(int i=0;i<marks.length;i++){
+        System.out.println("Enter the marks: ");
+        marks[i]=sc.nextInt();
+    }
+
+}
+}
