f30115cd73fa04c036de41e0b226a9a443ad7508
Anusigan Sivananthan <148673260+Anusigan@users.noreply.github.com>
2024-10-09 10:02:55 +0530

    circle

---
+++ src/Person.java
@@ -0,0 +1,17 @@
+public class Person {
+    private String name;
+    private String surname;
+    private int age;
+
+
+    public Person(String name) {
+        this.name=name;
+
+    }
+
+
+    public void displayName(){
+        System.out.println(name);
+    }
+
+}
---
+++ src/TutorialWeek02.java
@@ -0,0 +1,7 @@
+public class TutorialWeek02 {
+    public static void main(String[] args) {
+        System.out.println("Hello World");
+        Person p=new Person("Ben");
+        p.displayName();
+    }
+}
