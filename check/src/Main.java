2bd6bb0c3b743ebec13ffb27ebfc02e3c36bba91
Anusigan Sivananthan <148673260+Anusigan@users.noreply.github.com>
2024-10-16 09:58:25 +0530

    week 4 final ques

---
+++ check/src/Main.java
@@ -0,0 +1,14 @@
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void main(String[] args) {
+    IdentifyVariables a=new IdentifyVariables();
+    IdentifyVariables b=new IdentifyVariables();
+    a.y=5;
+    b.y=6;
+    a.x=1;
+    b.x=2;
+    System.out.println("a.y="+a.y);
+
+    }
+}
